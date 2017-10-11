a657446fd98b27d5976b396d1ab36d28188d936e
Justin Rhude <justin.rhude52@gmail.com>
2017-10-10 20:53:02 -0400

    Updated index and welcome pages
    to reflect the changes to account system

--- index.php
+++ index.php
@@ -5,28 +5,58 @@
         header('Location:welcome.php');
     }
 
+    if (isset($_POST['new_user'])){
+        header('Location:new_user.php');
+    }
+
     if (!isset($_SESSION['login_attempts_count'])) {
         $_SESSION['login_attempts_count'] = 0;
         $_SESSION['login_attempts'] = array();
     }
 
     if (isset($_POST['login'])) {
-        $passwords = array(
-            "hello" => "world",
-            "testing" => "123",
-            "atari" => "2600",
-            );
 
-        if (array_key_exists($_POST["username"], $passwords) && $_POST["password"] == $passwords[$_POST["username"]]){
-            $_SESSION['authenticated'] = true;
-            $_SESSION['username'] = $_POST['username'];
-            $_SESSION['password'] = $_POST['password'];
-            header('Location:welcome.php');
-        } else {
-            echo "<h2>The information you provided is incorrect.</h2>";
-            $_SESSION['login_attempts_count'] += 1;
-            array_push($_SESSION['login_attempts'], array($_POST["username"], $_POST['password']));
-        }
+        $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "cosc";
+
+            try {
+                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                $stmt = $conn->prepare("SELECT username, password_hash FROM users WHERE username='".$_POST['username']."'");
+                $stmt->execute();
+
+                $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                $fetch_result = $stmt->fetch();
+
+                if (count($fetch_result) == 0) {
+                    echo "<h2>The information you provided is incorrect.</h2>";
+                    $_SESSION['login_attempts_count'] += 1;
+                    array_push($_SESSION['login_attempts'], array($_POST["username"], $_POST['password']));
+                }
+                else {
+                    $hash = $fetch_result['password_hash'];
+
+                    if (password_verify($_POST['password'], $hash)){
+                        $_SESSION['authenticated'] = true;
+                        $_SESSION['username'] = $_POST['username'];
+                        $_SESSION['password'] = $hash;
+                        $conn = null;
+                        header('Location:welcome.php');
+                    }
+                    else {
+                        echo "<h2>The information you provided is incorrect.</h2>";
+                        $_SESSION['login_attempts_count'] += 1;
+                        array_push($_SESSION['login_attempts'], array($_POST["username"], $_POST['password']));
+                    }
+                }
+            }
+            catch(PDOException $e){
+                echo "Connection failed: " . $e->getMessage();
+            }
+            $conn = null;
     }
 ?>
 
@@ -45,6 +75,7 @@
 
 <form action="index.php" method="post" class="bottom-form">
         <button type="submit" name="report">Report</button>
+        <button type="submit" name="new_user">Create New User</button>
 </form>
 
 <?php 
@@ -62,7 +93,6 @@
                         <td>". $_SESSION['login_attempts'][$i][0] ."</td>
                         <td>". $_SESSION['login_attempts'][$i][1] ."</td>
                     </tr>";
-                
         }
         echo "</table>";
     }
