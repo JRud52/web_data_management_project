7b66625d1d137d840a9cd1fd64509be4381b5a3c
Justin Rhude <justin.rhude52@gmail.com>
2017-11-05 13:08:43 -0500

    updated views

---
+++ app/views/reminder/update.php
@@ -0,0 +1,40 @@
+<?php require_once '../app/views/templates/header.php'?>
+
+<div class="container">
+    <div class="page-header" id="banner">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Update Reminder</h1>
+                <p class="lead"> <?= date("F jS, Y"); ?></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+			<div class="col-lg-12">
+				<form class="form-horizontal" action="/reminder/update_reminder/1" method="post">
+			    <fieldset>
+						<input type="hidden" class="form-control" name="id" value="<?=$data['item']['id']?>" required>
+						<div class="form-group">
+							<label for="subject" class="col-lg-2 control-label">Subject</label>
+							<div class="col-lg-10">
+							<input type="text" class="form-control" name="subject" value="<?=$data['item']['subject']?>" required>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="desc" class="col-lg-2 control-label">Description</label>
+							<div class="col-lg-10">
+							<input type="text" class="form-control" name="desc" value="<?=$data['item']['description']?>" required>
+							</div>
+						</div>
+											<div class="form-group">
+							<div class="col-lg-10 col-lg-offset-2">
+							<button type="submit" class="btn btn-primary">Update</button>
+							</div>
+						</div>
+			    </fieldset>
+				</form>
+			</div>
+    </div>
+
+    <?php require_once '../app/views/templates/footer.php' ?>
